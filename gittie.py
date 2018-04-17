17f2cc777bc879c2d4cf88a2ccb0d492c9af9ff3
darDomanski <dardomanski@gmail.com>
2018-04-17 10:15:22 +0200

    Changed introduce mesagge.

--- gittie.py
+++ gittie.py
@@ -1,8 +1,8 @@
 def introduce():
-    print("Hello, I'm Gittie!")
+    print("Hello, I'm madafaka Gittie!")
 
 def addition(a,b)
     print(sum(a,b))
-    
+
 def joke():
     print("Dlaczego wróbelek? Bo jedną nóżkę ma bardzej!")
